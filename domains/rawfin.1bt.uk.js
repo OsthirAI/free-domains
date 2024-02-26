edb4ce77cd1e74dd48a48450ce561cefe702f484
Zaid Amin <hello@rawfin.net>
2024-02-27 00:16:36 +0600

    Create rawfin.1bt.uk.js

---
+++ domains/rawfin.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'My portfolio website',
+  domain: '1bt.uk',
+  subdomain: 'rawfin',
+  owner: {
+    email: 'hello@rawfin.net',
+  },
+  record: {
+    CNAME: 'rawfin.pages.dev'
+  },
+  proxy: false,
+})
